911a9cbdf0ca327b7d0dea9d199d061add800851
marparedes <m-ariana_98@hotmail.com>
2024-11-02 16:26:43 -0300

    add login logic, redux, router and remove node_modules

---
+++ frontend/src/services/api.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { getAuthToken } from './secureStore';
+
+const api = axios.create({
+  baseURL: 'http://10.0.2.2:5000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// export const login = (credentials) => authApi.post('/auth/login', credentials);
+// export const signup = (userData) => authApi.post('/auth/signup', userData);
+// export default authApi;
+// Interceptor para agregar el token de autenticación si está disponible
+api.interceptors.request.use(
+  async (config) => {
+    const token = await getAuthToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Interceptor para manejar la respuesta
+api.interceptors.response.use(
+  (response) => {
+    // Extraer solo los datos necesarios
+    return {
+      ...response,
+      data: response.data,
+    };
+  },
+  (error) => {
+    // Puedes manejar el error de la forma que necesites
+    return Promise.reject(error);
+  }
+);
+
+export default api;
