9bd6c608ea92a24e091dfa7dfbe8fa7a9388af92
marparedes <m-ariana_98@hotmail.com>
2024-11-14 20:41:58 -0300

    fix welcome

--- frontend/app/signup/welcome.jsx
+++ frontend/app/signup/welcome.jsx
@@ -8,11 +8,6 @@ const WELCOME = () => {
     return (
       <SafeAreaView style={styles.safeArea}>
         <StatusBar barStyle="dark-content" />
-        {/* {loading ? ( */}
-          <View style={styles.loadingContainer}>
-            <ActivityIndicator size="large" color="#B5432A" />
-          </View>
-        {/* ) : ( */}
         <View style={styles.imagesContainer}>
           <Image source={require('../../assets/images/Logo 2.png')} style={styles.logo} />
           <Text style={styles.title}>¡Te damos la bienvenida!</Text>
@@ -24,7 +19,6 @@ const WELCOME = () => {
             </TouchableOpacity>
   
         </View>
-        {/* )} */}
       </SafeAreaView>
   
   
