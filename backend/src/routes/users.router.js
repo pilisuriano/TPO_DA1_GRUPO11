363bae78c6cf97fa4f7e87d6d8e6fdf30820b81b
marparedes <m-ariana_98@hotmail.com>
2024-12-03 09:20:25 -0300

    fix

--- backend/src/routes/users.router.js
+++ backend/src/routes/users.router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { searchUser, getMe, updateMe, deleteMe, getOtherUserProfile, getOtroUsuario, getFavoritesPosts, getFollowers, getFollowing, followUser, unfollowUser } from "../controllers/user.controller.js";
+import { searchUser, getMe, updateMe, deleteMe, getOtherUserProfile, getFavoritesPosts, getFollowers, getFollowing, followUser, unfollowUser } from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
 const usersRoutes = express.Router();
@@ -11,7 +11,6 @@ usersRoutes.put("/me", protectRoute, updateMe);
 usersRoutes.delete("/me", protectRoute, deleteMe);
 usersRoutes.get("/me/favorites", protectRoute, getFavoritesPosts)
 usersRoutes.get("/:userId", protectRoute, getOtherUserProfile);
-usersRoutes.get("/:userId", protectRoute, getOtroUsuario);
 usersRoutes.get("/:userId/followers", protectRoute, getFollowers)
 usersRoutes.get("/:userId/following", protectRoute, getFollowing)
 usersRoutes.post("/:userId/follow", protectRoute, followUser)
